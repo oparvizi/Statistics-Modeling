b90e8376ac4934dd3be64bdf1246706bfe7b5730
O <105786517+oparvizi@users.noreply.github.com>
2023-03-16 23:21:01 +0100

    Add files via upload

--- Meta Analysis/MetaAnalysis.R
+++ Meta Analysis/MetaAnalysis.R
@@ -9,37 +9,43 @@ if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-proj
 
 # Define UI ----
 ui <- fluidPage(
-  titlePanel("Meta Analysis in R"),
-  sidebarLayout(
-    sidebarPanel(
-      h4("Description"),
-      p("An attempt to provide an example of meta-analysis in R using the meta-library."),
-      HTML(paste0(
-        "<br>",
-        "<img style = 'display: block; margin-left: auto;margin-right: auto;' src='https://www.svgrepo.com/show/245581/learning.svg'; width = '50'; height='50'; background-color: white;>",
-        "<script>",
-        "var today = new Date();",
-        "var yyyy = today.getFullYear();",
-        "</script>",
-        "<p style = 'text-align: center;'><small>&copy; - <a href='https://github.com/oparvizi/' target='_blank'>Statistics & Modeling</a> - <script>document.write(yyyy);</script></small></p>"
-      )),
-    ),
-    mainPanel(
-      fluidRow( 
-        box(title = "Dataset: ", width = 12, height = "auto", reactableOutput("table")),
-        box(title = "Meta Analysis: ", width = 12, height = "auto", verbatimTextOutput("summary1")),
-        box(title = "Forest Plots:", width = 12, height = "auto", plotOutput("plot1")),
-        box(title = "Funnel Plots:", width = 12, height = "auto", plotOutput("plot2")),
-        box(title = "Linear regression test of funnel plot asymmetry:", width = 12, height = "auto", verbatimTextOutput("summary2")),
-        box(title = "", width = 12, height = "auto", plotOutput("plot3")),
-
-      )
-    )
+  HTML(paste0(
+    "<br>",
+    "<img style = 'display: block; margin-left: auto;margin-right: auto;' src='https://www.svgrepo.com/show/245581/learning.svg'; width = '50'; height='50'; background-color: white;>",
+    "<script>",
+    "var today = new Date();",
+    "var yyyy = today.getFullYear();",
+    "</script>",
+    "<p style = 'text-align: center;'><small>&copy; - <a href='https://github.com/oparvizi/' target='_blank'>Statistics & Modeling</a> - <script>document.write(yyyy);</script></small></p>"
+  )),
+  navbarPage("Meta Analysis in R",
+             tabPanel("Data description", br(),br(),
+                      box(title = "Dataset: ", width = 12, height = "auto", reactableOutput("table")),br(),br(),
+                      box(title = "Meta Analysis: ", width = 12, height = "auto", verbatimTextOutput("summary1")),
+             ),
+             tabPanel("Methods analysis", br(),br(),
+                      box(title = "Forest Plots:", width = 8, height = "auto", plotOutput("plot1")),
+                      box(title = "Funnel Plots:", width = 8, height = "auto", plotOutput("plot2")),
+                      box(title = "Linear regression test of funnel plot asymmetry:", width = 12, height = "auto", verbatimTextOutput("summary2")),
+                      box(title = "", width = 8, height = "auto", plotOutput("plot3")),
+             ),
+             tabPanel("About...", br(),br(),
+                      verbatimTextOutput("us")
+             ),
   )
 )
 
 # Define server logic ----
 server <- function(input, output) {
+  
+  
+  library(meta)
+  dat <- data("Fleiss1993cont")
+  res.flesiss =  metacont(n.psyc, mean.psyc, sd.psyc, 
+                          n.cont, mean.cont, sd.cont,
+                          comb.fixed = T, comb.random = T, studlab = study,
+                          data = Fleiss1993cont, sm = "SMD") 
+  res.flesiss
   
   ##### CALCULATION
   ##----------------------------------------------------------------------------
@@ -47,24 +53,19 @@ server <- function(input, output) {
   ##----------------------------------------------------------------------------
   # LIBRARY meta----------------------------------------------------------------
   # source: https://cran.r-project.org/web/packages/meta/index.html
-  #         https://bookdown.org/MathiasHarrer/Doing_Meta_Analysis_in_R/
+  #          https://bookdown.org/MathiasHarrer/Doing_Meta_Analysis_in_R/
   #         https://www.r-bloggers.com/2021/08/meta-analysis-in-r/
-  
-  library(meta)
-  dat <- data("Fleiss1993cont")
-
 
   # Descriptive statistics and plots
   library(reactable)
   output$table = renderReactable({
     data("Fleiss1993cont")
-    saveRDS(Fleiss1993cont, file = "Fleiss1993cont.Rds")
     reactable(
       Fleiss1993cont[1:length(Fleiss1993cont$study), ],
       searchable = TRUE,
       filterable = TRUE,
       defaultPageSize = 5,
-      paginationType = "simple",
+      paginationType = "jump",
       language = reactableLang(
         searchPlaceholder = "Search...",
         noData = "No entries found",
@@ -81,14 +82,9 @@ server <- function(input, output) {
     )
   })
   
-  res.flesiss =  metacont(n.psyc, mean.psyc, sd.psyc, 
-                          n.cont, mean.cont, sd.cont,
-                          comb.fixed = T, comb.random = T, studlab = study,
-                          data = Fleiss1993cont, sm = "SMD") 
-  res.flesiss
   # Descriptive statistics and plots
-  output$summary1 <- renderPrint({
-    # Description of the data--------------
+  output$summary1 <- renderPrint({#---------------------------------------------
+    # Description of the data
     # meta-analysis with continuout outcome
     # comb.fixed/comb.random: indicator whether a fix/random effect mata-analysis to be conducted.
     # sm: Three different types of summary measures to choose,standardized mean difference (SMD),mean difference (MD), ratio of means (ROM)
@@ -99,16 +95,16 @@ server <- function(input, output) {
     res.flesiss
    })    
   
-  # Forest---------------------------------------- 
+  # Forest----------------------------------------------------------------------
   output$plot1 <- renderPlot({
     forest(res.flesiss, leftcols = c('studlab'))
   })
   
-  # Funnel Plot---------------------------------------- 
+  # Funnel Plot----------------------------------------------------------------- 
   output$plot2 <- renderPlot({
     funnel(res.flesiss)
   })
-  #---------------------------------------- 
+  #----------------------------------------------------------------------------- 
   output$summary2 <- renderPrint({  
     res.flesiss =  metacont(n.psyc, mean.psyc, sd.psyc, 
                             n.cont, mean.cont, sd.cont,
@@ -117,7 +113,7 @@ server <- function(input, output) {
     res.flesiss
     metabias(res.flesiss, method.bias = 'linreg', k.min = 5, plotit = T)
   })
-  #---------------------------------------- 
+  #----------------------------------------------------------------------------- 
   output$plot3 <- renderPlot({
     metabias(res.flesiss, method.bias = 'linreg', k.min = 5, plotit = T)
   })
